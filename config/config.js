ee38244b53bf024758afa4d9d5f580b4b02539cc
Parvathi Kumar <parvathi.kumar1989@gmail.com>
2023-06-21 16:46:14 +0530

    ready

--- config/config.js
+++ config/config.js
@@ -4,8 +4,11 @@ const mongoose=require('mongoose');
 const mongooseUp=()=>{   
     require('dotenv').config(); 
 
-    mongoose.set('strictQuery',false);        
-    mongoose.connect(process.env.DBURL);
+    mongoose.set('strictQuery',true);        
+    mongoose.connect(process.env.DBURL, {
+        serverSelectionTimeoutMS: 5000,
+        socketTimeoutMS: 30000,
+      });
     mongoose.connection.once('open',()=>{
     console.log('connection is successfully established');
 });
